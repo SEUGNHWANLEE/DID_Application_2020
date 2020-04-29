bcd6532e7f188b62c5c24fa7a5786b93f86f0eb4
jonghapark <59429710+jonghapark@users.noreply.github.com>
2020-04-29 16:15:00 +0900

    Update index.js

--- aham_practice/app/router/index.js
+++ aham_practice/app/router/index.js
@@ -1,10 +1,12 @@
 import Main from '../components/Main';
 import PasswordPage from '../components/PasswordPage';
 import IdentificationPage from '../components/IdentificationPage';
+import IdentifyEmail from '../components/IdentifyEmail'
 
 const router = {
     main: Main,
     password: PasswordPage,
-    identify: IdentificationPage
+    identify: IdentificationPage,
+    identifyEmail : IdentifyEmail
 }
 export default router
