acd017926490884d9f633b422a897eb4843db208
jonghapark <59429710+jonghapark@users.noreply.github.com>
2020-04-30 12:25:16 +0900

    verify

--- aham_practice/app/components/IdentificationPage.vue
+++ aham_practice/app/components/IdentificationPage.vue
@@ -12,29 +12,33 @@
     <StackLayout>
       <TabView :selectedIndex="selectedIndex" iosIconRenderingMode="alwaysOriginal">
         <TabViewItem title="Personal ID">
-          <Button text="ID : 개인정보 하라고" @tap="goToEmail" />
+          <Button text="ID : 개인정보 하라고yo" @tap="goToEmail()" />
         </TabViewItem>
         <TabViewItem title="여권정보">
-          <Button text="여권 : 개인정보 입력" />
+          <Button text="여권 : 개인정보 입력" @tap="goToEmail()" />
         </TabViewItem>
 
         <TabViewItem title="학생증" >
-          <Button text="학생증 : 개인정보 입력" />
+          <Button text="학생증 : 개인정보 입력" @tap="goToEmail()" />
         </TabViewItem>
       </TabView>
 
     </StackLayout>
   </Page>
 </template>
-
 <script>
+
+
+import Identifyemail from "./IdentifyEmail";
+
 export default {
   methods:{
     goToEmail(){
-      confirm('메일인증을 진행합니다.')
+      confirm('메일인증을 진행합니다 !')
       .then(result => {
       if (result == true){
-        $goto('identifyEmail',navOptions);
+        console.log(result);
+        this.$navigateTo(Identifyemail);
       }
       else console.log(result);
 })
