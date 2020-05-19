f9738a0bc80c3b7783295cceba2cc1d1b3c987f6
Seunghwanly <seunghwanly@gmail.com>
2020-05-19 23:37:23 +0900

    passport update

--- aham_practice/app/components/IdentificationPage.vue
+++ aham_practice/app/components/IdentificationPage.vue
@@ -1,49 +1,54 @@
 <template>
   <Page>
     <ActionBar title="Service">
-      <ActionItem text="Go Back"
+      <ActionItem
+        text="Go Back"
         @tap="$goto('main',navOptions)"
         ios.position="left"
         android.systemIcon="ic_menu_back"
       />
     </ActionBar>
-     
+
     <StackLayout>
       <TabView :selectedIndex="selectedIndex" iosIconRenderingMode="alwaysOriginal">
         <TabViewItem title="Personal ID">
           <Button text="ID : 개인정보 등록" @tap="goToEmail()" />
         </TabViewItem>
         <TabViewItem title="여권정보">
-          <Button text="여권 : 개인정보 입력" @tap="goToEmail()" />
+          <Button text="여권 : 개인정보 입력" @tap="goToPassport()" />
         </TabViewItem>
 
-        <TabViewItem title="학생증" >
+        <TabViewItem title="학생증">
           <Button text="학생증 : 개인정보 입력" @tap="goToEmail()" />
         </TabViewItem>
       </TabView>
-
     </StackLayout>
   </Page>
 </template>
 <script>
-
-
 import Identifyemail from "./IdentifyEmail";
+import IdentifyPassport from "./IdentifyPassport";
 
 export default {
-  methods:{
-    goToEmail(){
-      confirm('메일인증을 진행합니다 !')
-      .then(result => {
-      if (result == true){
-        console.log(result);
-        this.$navigateTo(Identifyemail);
-      }
-      else console.log(result);
-})
-
+  methods: {
+    goToEmail() {
+      confirm("메일인증을 진행합니다 !").then(result => {
+        if (result == true) {
+          console.log(result);
+          this.$navigateTo(Identifyemail);
+        } else console.log(result);
+      });
+    },
+    goToPassport() {
+      confirm(
+        "여권정보를 입력합니다 !".then(response => {
+          if (response == true) {
+            console.log("user pressed : " + response);
+            this.$$navigateTo(IdentifyPassport);
+          } else console.log(error);
+        })
+      );
     }
-
   },
   computed: {
     navOptions() {
--- aham_practice/app/components/IdentifyPassport.vue
+++ aham_practice/app/components/IdentifyPassport.vue
@@ -2,14 +2,39 @@
     <Page>
         <FlexboxLayout class="background">
             <FlexboxLayout flexDirection="column">
-                
+                <TextField id="info" v-model="Type" hint="종류 / Type"></TextField>
+                <TextField id="info" v-model="PassportNo" hint="여권번호 / Passport No."></TextField>
+                <TextField id="info" v-model="Surname" hint="성 / Surname"></TextField>
+                <TextField id="info" v-model="GivenNames" hint="이름 / Given Names"></TextField>
+                <TextField id="info" v-model="Nationality" hint="국적 / Nationality"></TextField>
+                <TextField id="info" v-model="Bdate" hint="생년월일 / Date of birth"></TextField>
+                <TextField id="info" v-model="Sex" hint="성별 / Sex"></TextField>
+                <TextField id="info" v-model="Idate" hint="발급일 / Date of issue"></TextField>
+                <TextField id="info" v-model="Edate" hint="만료일 / Date of expiry"></TextField>
+                <TextField id="info" v-model="PersonalNo" hint="주민등록번호 뒷자리 / Personal No."></TextField>
+                <Button id="submit" text="확인"></Button>
             </FlexboxLayout>
         </FlexboxLayout>
     </Page>
 </template>
 <script>
 export default {
-    
+    data(){
+        return {
+            Passport : {
+                Type:"",
+                PassportNo:"",
+                Surname:"",
+                GivenNames:"",
+                Nationality:"",
+                Bdate:"",
+                Sex:"",
+                Idate:"",
+                Edate:"",
+                PersonalNo:"",
+            }
+        }
+    }
 }
 </script>
 <style scoped>
