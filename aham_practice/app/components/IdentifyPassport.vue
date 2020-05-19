362bd756ea0dbf99202d4b3d0085d688c1cfde68
Seunghwanly <seunghwanly@gmail.com>
2020-05-20 01:31:09 +0900

    update form

--- aham_practice/app/components/IdentifyPassport.vue
+++ aham_practice/app/components/IdentifyPassport.vue
@@ -1,42 +1,98 @@
 <template>
-    <Page>
-        <FlexboxLayout class="background">
-            <FlexboxLayout flexDirection="column">
-                <TextField id="info" v-model="Type" hint="종류 / Type"></TextField>
-                <TextField id="info" v-model="PassportNo" hint="여권번호 / Passport No."></TextField>
-                <TextField id="info" v-model="Surname" hint="성 / Surname"></TextField>
-                <TextField id="info" v-model="GivenNames" hint="이름 / Given Names"></TextField>
-                <TextField id="info" v-model="Nationality" hint="국적 / Nationality"></TextField>
-                <TextField id="info" v-model="Bdate" hint="생년월일 / Date of birth"></TextField>
-                <TextField id="info" v-model="Sex" hint="성별 / Sex"></TextField>
-                <TextField id="info" v-model="Idate" hint="발급일 / Date of issue"></TextField>
-                <TextField id="info" v-model="Edate" hint="만료일 / Date of expiry"></TextField>
-                <TextField id="info" v-model="PersonalNo" hint="주민등록번호 뒷자리 / Personal No."></TextField>
-                <Button id="submit" text="확인"></Button>
-            </FlexboxLayout>
+  <Page>
+    <FlexboxLayout class="background">
+      <FlexboxLayout width="*" height="*" flexDirection="column">
+        <FlexboxLayout>
+          <label>종류 / Type</label>
+          <TextField id="info" v-model="Type" hint="PM"></TextField>
         </FlexboxLayout>
-    </Page>
+        <FlexboxLayout>
+          <label>여권번호 / Passport No.</label>
+          <TextField id="info" v-model="PassportNo" hint="M12312345"></TextField>
+        </FlexboxLayout>
+        <FlexboxLayout>
+          <label>성 / Surname</label>
+          <TextField id="info" v-model="Surname" hint="HONG"></TextField>
+        </FlexboxLayout>
+        <FlexboxLayout>
+          <label>이름 / Given Names</label>
+          <TextField id="info" v-model="GivenNames" hint="GIL DONG"></TextField>
+        </FlexboxLayout>
+        <FlexboxLayout>
+          <label>국적 / Nationality</label>
+          <TextField id="info" v-model="Nationality" hint="REPUBLIC OF KOREA"></TextField>
+        </FlexboxLayout>
+        <FlexboxLayout>
+          <label>생년월일 / Date of birth</label>
+          <TextField id="info" v-model="Bdate" hint="17 SEP 1996"></TextField>
+        </FlexboxLayout>
+        <FlexboxLayout>
+          <label>성별 / Sex</label>
+          <TextField id="info" v-model="Sex" hint="M / F"></TextField>
+        </FlexboxLayout>
+        <FlexboxLayout>
+          <label>발급일 / Date of issue</label>
+          <TextField id="info" v-model="Idate" hint="21 APR 2020"></TextField>
+        </FlexboxLayout>
+        <FlexboxLayout>
+          <label>만료일 / Date of expiry</label>
+          <TextField id="info" v-model="Edate" hint="30 DEC 2025"></TextField>
+        </FlexboxLayout>
+        <FlexboxLayout>
+          <label>주민등록번호 / Personal No.</label>
+          <TextField id="info" v-model="PersonalNo" hint="1234567"></TextField>
+        </FlexboxLayout>
+
+        <Button id="submit" text="확인" @tap="submit"></Button>
+      </FlexboxLayout>
+    </FlexboxLayout>
+  </Page>
 </template>
 <script>
 export default {
-    data(){
-        return {
-            Passport : {
-                Type:"",
-                PassportNo:"",
-                Surname:"",
-                GivenNames:"",
-                Nationality:"",
-                Bdate:"",
-                Sex:"",
-                Idate:"",
-                Edate:"",
-                PersonalNo:"",
-            }
+  data() {
+    return {
+      Passport: {
+        Type: "",
+        PassportNo: "",
+        Surname: "",
+        GivenNames: "",
+        Nationality: "",
+        Bdate: "",
+        Sex: "",
+        Idate: "",
+        Edate: "",
+        PersonalNo: ""
+      }
+    };
+  },
+  methods: {
+    submit: function() {
+      confirm("제출하시겠습니까?").then(res => {
+        if (res == true) {
+          console.log("true pressed");
+        } else {
+          console.log("false pressed");
         }
+      });
     }
-}
+  }
+};
 </script>
 <style scoped>
-
+.background {
+  width: 100%;
+  height: 70%;
+  background: white;
+}
+label {
+  width: 150;
+  height: 30;
+  size: 10;
+}
+#info {
+  width: 150;
+  height: 30;
+  color: darkgray;
+}
 </style>
